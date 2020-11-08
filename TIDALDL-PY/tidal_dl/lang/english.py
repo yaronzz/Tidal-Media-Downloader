a90215260075ecc9b1a62d8d3e5547b6f43b64af
sporek <74082356+sporek@users.noreply.github.com>
2020-11-07 18:58:05 -0800

    OAuth2
    Added support for OAuth2 changes

--- TIDALDL-PY/tidal_dl/lang/english.py
+++ TIDALDL-PY/tidal_dl/lang/english.py
@@ -37,9 +37,9 @@ class LangEnglish(object):
     CHOICE_ENTER = "Enter"
     CHOICE_ENTER_URLID = "Enter 'Url/ID':"
     CHOICE_EXIT = "Exit"
-    CHOICE_LOGIN = "Login"
+    CHOICE_LOGIN = "Check AccessToken"
     CHOICE_SETTINGS = "Settings"
-    CHOICE_SET_ACCESS_TOKEN = "Set AccessToken"
+    #CHOICE_SET_ACCESS_TOKEN = "Set AccessToken"
     CHOICE_DOWNLOAD_BY_URL = "Download by url or id"
 
     PRINT_ERR = "[ERR]"
@@ -48,8 +48,8 @@ class LangEnglish(object):
 
     PRINT_ENTER_CHOICE = "Enter Choice:"
     PRINT_LATEST_VERSION = "Latest version:"
-    PRINT_USERNAME = "username:"
-    PRINT_PASSWORD = "password:"
+    #PRINT_USERNAME = "username:"
+    #PRINT_PASSWORD = "password:"
     
     CHANGE_START_SETTINGS = "Start settings('0'-Return,'1'-Yes):"
     CHANGE_DOWNLOAD_PATH = "Download path('0' not modify):"
@@ -69,8 +69,16 @@ class LangEnglish(object):
     CHANGE_ALBUM_FOLDER_FORMAT = "Album folder format('0' not modify):"
     CHANGE_TRACK_FILE_FORMAT = "Track file format('0' not modify):"
     CHANGE_SHOW_PROGRESS = "Show progress('0'-No,'1'-Yes):"
-
-    MSG_INVAILD_ACCESSTOKEN = "Invalid AccessToken! Please reset."
+    
+    # {} are required in these strings
+    AUTH_START_LOGIN = "Starting login process..."
+    AUTH_LOGIN_CODE = "Your login code is {}"
+    AUTH_NEXT_STEP = "Go to {} within the next {} to complete setup."
+    AUTH_WAITING = "Waiting for authorization..."
+    AUTH_TIMEOUT = "Operation timed out."
+    
+    MSG_VALID_ACCESSTOKEN = "AccessToken good for {}."
+    MSG_INVAILD_ACCESSTOKEN = "Expired AccessToken. Attempting to refresh it."
     MSG_PATH_ERR = "Path is error!"
     MSG_INPUT_ERR = "Input error!"
 
