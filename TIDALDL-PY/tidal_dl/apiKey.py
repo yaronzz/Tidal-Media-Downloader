2ac5f8f1527f5f2077b0f8ccc1d24f58f10ad0da
bladeoner <bladeoner112@gmail.com>
2022-02-27 14:55:04 +0100

    Cleanup api keys

--- TIDALDL-PY/tidal_dl/apiKey.py
+++ TIDALDL-PY/tidal_dl/apiKey.py
@@ -16,12 +16,12 @@ __KEYS_JSON__ = '''
     "version": "1.0.1",
     "keys": [
         {
-            "platform": "Fire TV",
-            "formats": "Normal/High/HiFi(No Master)",
-            "clientId": "OmDtrzFgyVVL6uW56OnFA2COiabqm",
-            "clientSecret": "zxen1r3pO0hgtOC7j6twMo9UAqngGrmRiWpV7QC1zJ8=",
-            "valid": "False",
-            "from": "Fokka-Engineering (https://github.com/Fokka-Engineering/libopenTIDAL/blob/655528e26e4f3ee2c426c06ea5b8440cf27abc4a/README.md#example)"
+            "platform": "Android Auto",
+            "formats": "Normal/High/HiFi/Master",
+            "clientId": "zU4XHVVkc2tDPo4t",
+            "clientSecret": "VJKhDFqJPqvsPVNBV6ukXTJmwlvbttP7wlMlrc72se4=",
+            "valid": "True",
+            "from": "1nikolas (https://github.com/yaronzz/Tidal-Media-Downloader/pull/840)"
         },
         {
             "platform": "Fire TV",
@@ -30,30 +30,6 @@ __KEYS_JSON__ = '''
             "clientSecret": "vRAdA108tlvkJpTsGZS8rGZ7xTlbJ0qaZ2K9saEzsgY=",
             "valid": "True",
             "from": "Dniel97 (https://github.com/Dniel97/RedSea/blob/4ba02b88cee33aeb735725cb854be6c66ff372d4/config/settings.example.py#L68)"
-        },
-        {
-            "platform": "Android TV",
-            "formats": "Normal/High/HiFi(No Master)",
-            "clientId": "Pzd0ExNVHkyZLiYN",
-            "clientSecret": "W7X6UvBaho+XOi1MUeCX6ewv2zTdSOV3Y7qC3p3675I=",
-            "valid": "False",
-            "from": ""
-        },
-        {
-            "platform": "TV",
-            "formats": "Normal/High/HiFi/Master",
-            "clientId": "8SEZWa4J1NVC5U5Y",
-            "clientSecret": "owUYDkxddz+9FpvGX24DlxECNtFEMBxipU0lBfrbq60=",
-            "valid": "False",
-            "from": "morguldir (https://github.com/morguldir/python-tidal/commit/50f1afcd2079efb2b4cf694ef5a7d67fdf619d09)"
-        },
-        {
-            "platform": "Android Auto",
-            "formats": "Normal/High/HiFi/Master",
-            "clientId": "zU4XHVVkc2tDPo4t",
-            "clientSecret": "VJKhDFqJPqvsPVNBV6ukXTJmwlvbttP7wlMlrc72se4=",
-            "valid": "True",
-            "from": "1nikolas (https://github.com/yaronzz/Tidal-Media-Downloader/pull/840)"
         }
     ]
 }
