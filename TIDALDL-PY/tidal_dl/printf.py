4e8751ceb15a92530257471f932cb53aa43acc8c
Yaronzz <yaronhuang@foxmail.com>
2022-10-12 10:28:26 +0800

    upload new version

--- TIDALDL-PY/tidal_dl/printf.py
+++ TIDALDL-PY/tidal_dl/printf.py
@@ -22,7 +22,7 @@ from tidal_dl.settings import *
 from tidal_dl.lang.language import *
 
 
-VERSION = '2022.08.29.1'
+VERSION = '2022.10.12.1'
 __LOGO__ = f'''
  /$$$$$$$$ /$$       /$$           /$$               /$$ /$$
 |__  $$__/|__/      | $$          | $$              | $$| $$
